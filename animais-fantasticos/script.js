6c67dfe94ceab8d0cef5621a8d95f250b8c1125a
miguel sanglard <miguelsanglard@gmail.com>
2024-02-16 07:42:41 -0300

    seleção-de-elementos

---
+++ animais-fantasticos/script.js
@@ -0,0 +1,17 @@
+
+const imagens = document.querySelectorAll('img')
+console.log(imagens)
+
+const imagem = document.querySelectorAll('[src^="img/imagem"]')
+console.log(imagem)
+
+
+const links = document.querySelectorAll('[href^="#"]')
+console.log(links)
+
+const h2EmAnimais = document.querySelector('.animais-descricao h2')
+console.log(h2EmAnimais)
+
+const paragrafo = document.querySelectorAll('p')
+
+console.log(paragrafo[paragrafo.length - 1])
