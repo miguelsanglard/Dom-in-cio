b34ee5ca8a46748807806b9dfa53afc9ccb2a0cf
miguel sanglard <miguelsanglard@gmail.com>
2024-02-15 23:53:33 -0300

    o que é dom

---
+++ o que é dom/script.js
@@ -0,0 +1,14 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href;
+console.log(url);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const selecionar = document.querySelector('.ativo')
+console.log(selecionar.innerHTML)
+// Retorne a linguagem do navegador
+const lingua = window.navigator.language;
+console.log(lingua)
+// Retorne a largura da janela 
+const largura = window.innerWidth;
+console.log(largura)
