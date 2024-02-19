51995caa1263067237c83ebcda6bd5afeb556e0c
miguel sanglard <miguelsanglard@gmail.com>
2024-02-19 01:34:10 -0300

    according-list

---
+++ accordion-list/script.js
@@ -0,0 +1,35 @@
+function initTavNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabcontent = document.querySelectorAll('.js-tabcontent section');
+
+  if (tabMenu.length && tabcontent.length) {
+    tabcontent[0].classList.add('ativo')
+    function activeTab(index) {
+      tabcontent.forEach((section) => {
+        section.classList.remove('ativo')
+      });
+      tabcontent[index].classList.add('ativo');
+    }
+    tabMenu.forEach((menuItem, index) => {
+      menuItem.addEventListener('click', () => {
+        activeTab(index)
+      })
+    })
+  }
+}
+initTavNav()
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  const ativo = 'ativo'
+  accordionList[0].classList.add(ativo);
+  accordionList[0].nextElementSibling.classList.add(ativo);
+  function activeAccordion() {
+    this.classList.toggle(ativo)
+    this.nextElementSibling.classList.toggle(ativo)
+  }
+
+  accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+  })
+}
+initAccordion()
